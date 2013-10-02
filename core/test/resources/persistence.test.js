dd98dd134d99ef579a1c686f083c01b9e23ce703
Nicolas Herment <nherment@arkhaios.net>
2013-10-02 13:24:39 -0700

    Added a unit test for the messaging persistence serialization mechanism

--- core/test/resources/persistence.test.js
+++ core/test/resources/persistence.test.js
@@ -29,5 +29,58 @@ exports['given a resource'] = {
       done();
     })
 
+  },
+
+  'persisting messages serializes it when the message is an object': function(done) {
+    // (nherment) TODO: the result should actually be deserialized because it is being serialized in persistOrdered()
+    // The problem is that radar_client currently deserializes the response.
+    // We need to make the client not deserialize the response so that we can deserialize it here.
+
+
+    var key = "persistence.messages.object.test";
+    var objectValue = {
+      foo: "bar"
+    };
+
+    Persistence.persistOrdered(key, objectValue, function(err) {
+      if(err) {
+        return done(err);
+      }
+      Persistence.readOrderedWithScores(key, undefined, function(replies) {
+
+        assert(replies instanceof Array)
+        assert.equal(2, replies.length)
+        assert.equal("string", typeof replies[0])
+        assert.equal(JSON.stringify(objectValue), replies[0])
+        done();
+      })
+
+    })
+  },
+
+  'persisting messages serializes it when the message is a string': function(done) {
+    // (nherment) TODO: the result should actually be deserialized because it is being serialized in persistOrdered()
+    // The problem is that radar_client currently deserializes the response.
+    // We need to make the client not deserialize the response so that we can deserialize it here.
+
+
+    var key = "persistence.messages.string.test";
+    var stringValue = "Hello World"
+
+    Persistence.persistOrdered(key, stringValue, function(err) {
+      if(err) {
+        return done(err);
+      }
+      Persistence.readOrderedWithScores(key, undefined, function(replies) {
+
+        assert(replies instanceof Array)
+        assert.equal(2, replies.length)
+        assert.equal("string", typeof replies[0])
+        assert.equal(JSON.stringify(stringValue), replies[0])
+        done();
+      })
+
+    })
   }
+
 };
