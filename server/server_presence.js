fedc272017aa4e1d639d9f558d95e9c1cde71994
Vanchi Koduvayur <vanchi@zendesk.com>
2014-02-18 21:01:58 -0800

    changes to serverPresence

--- server/server_presence.js
+++ server/server_presence.js
@@ -1,34 +1,40 @@
 var Core = require('../core'),
     logging = require('minilog')('server:server_presence');
 
-var RADAR_SERVER_EXPIRY = 15*1000,
-    scope, port, heartbeat;
+var radarServerExpiry = 15*1000,
+    serverPresenceId, port, heartbeat,
+    prefix = 'radar_server_presence:/';
 
 function keepAlive() {
-  var scope = server_presence_scope(port);
+  var scope = prefix + getServerPresenceId();
 
   logging.info("keep-alive ", scope, new Date());
   Core.Persistence.persistHash(scope, 'last_seen_at', Date.now());
-  Core.Persistence.expire(scope, 2*RADAR_SERVER_EXPIRY/1000);
+  Core.Persistence.expire(scope, 2*radarServerExpiry/1000);
 }
 
-function server_presence_scope() {
-  if(!scope) {
-    scope = 'radar_server_presence:/';
-    scope += require('os').hostname() + '/';
-    scope += port;
-    logging.info("Got new scope: ",scope);
+function getServerPresenceId() {
+  if(!serverPresenceId) {
+    serverPresenceId = require('os').hostname() + '/';
+    serverPresenceId += port + '/';
+    serverPresenceId += Date.now(); //Changes every restart
+    logging.debug("Got new serverPresenceId: ",serverPresenceId);
   }
-  return scope;
+  return serverPresenceId;
+}
+
+function fetchServerPresence(id, callback) {
+  Core.Persistence.exists(prefix+id, callback);
 }
 
 module.exports = {
-  server_expiry_scope: server_presence_scope,
+  getServerPresenceId: getServerPresenceId,
   setup: function(config_port) {
     logging.info("Setting up keep-alive");
     port = config_port;
     keepAlive();
-    heartbeat = setInterval(keepAlive, RADAR_SERVER_EXPIRY);
+    heartbeat = setInterval(keepAlive, radarServerExpiry);
   },
+  fetchServerPresence: fetchServerPresence,
   heartbeat: heartbeat
 }
