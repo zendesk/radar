394dd0984c31204e480f5690745bfadc5339b8ff
Vanchi Koduvayur <vanchi@zendesk.com>
2014-04-04 21:40:13 -0700

    simplify client.auth.test

--- tests/client.auth.test.js
+++ tests/client.auth.test.js
@@ -1,29 +1,29 @@
 var http = require('http'),
-  assert = require('assert'),
-  Radar = require('../server/server.js'),
-  Client = require('radar_client').constructor,
-  Type = require('../core').Type,
-  common = require('./common.js'),
-  Tracker = require('callback_tracker'),
-  Persistence = require('../core/lib/persistence.js');
-var radar;
+    assert = require('assert'),
+    Radar = require('../server/server.js'),
+    Client = require('radar_client').constructor,
+    Type = require('../core').Type,
+    common = require('./common.js'),
+    Tracker = require('callback_tracker'),
+    Persistence = require('../core/lib/persistence.js');
+
+var radar, client;
 
 exports['auth test'] = {
   before: function(done) {
-    var self = this;
     radar = common.spawnRadar();
     radar.sendCommand('start', common.configuration, function() {
-      self.client = common.getClient('client_auth', 111, 0,
+      client = common.getClient('client_auth', 111, 0,
         { name: 'tester0' }, done);
     });
   },
 
   afterEach: function() {
-    this.client.message('test').removeAllListeners();
+    client.message('test').removeAllListeners();
   },
 
   after: function(done){
-    this.client.dealloc('test');
+    client.dealloc('test');
     radar.sendCommand('stop', {}, function() {
       radar.kill();
       done();
@@ -31,34 +31,35 @@ exports['auth test'] = {
   },
 
   // GET /radar/message?accountName=test&scope=chat/1
-  'should not succeed if type is disabled (should return err message)': function(done) {
-    var self = this;
+  'should emit err event if type is disabled': function(done) {
 
-    this.client.on('err', function(message) {
+    client.on('err', function(message) {
       assert.ok(message.origin);
       assert.equal(message.origin.op, 'subscribe');
       assert.equal(message.origin.to, 'message:/client_auth/disabled');
-      done();
+      setTimeout(done, 10);
     });
 
     //Messages of the form 'disabled' are disabled
-    self.client.message('disabled').subscribe();
+    client.message('disabled').subscribe(function() {
+      assert.ok(false);
+    });
   },
 
-  'should  succeed if type is enabled, no err message emitted': function(done) {
-    var self = this;
+  'should work if type is enabled': function(done) {
     var originalMessage = { hello: 'world', timestamp: Date.now()};
 
-    this.client.message('enabled').on(function(message) {
+    client.message('enabled').on(function(message) {
       assert.deepEqual(message.value, originalMessage);
+      assert.equal(message.to,'message:/client_auth/enabled');
       done();
     });
 
-    this.client.on('err', function(message) {
+    client.on('err', function(message) {
       assert.ok(false);
     });
 
     //Messages of the form 'disabled' are disabled
-    self.client.message('enabled').subscribe().publish(originalMessage);
+    client.message('enabled').subscribe().publish(originalMessage);
   },
 };
