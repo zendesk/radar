162f9bf81ae386ab20c9665b414aa0075cb4a379
Vanchi Koduvayur <vanchi@zendesk.com>
2014-04-04 18:12:37 -0700

    simplify message subscription test

--- tests/client.message.subscription.test.js
+++ tests/client.message.subscription.test.js
@@ -3,24 +3,23 @@ var common = require('./common.js'),
     Persistence = require('../core').Persistence,
     Client = require('radar_client').constructor,
     Tracker = require('callback_tracker'),
-    radar;
+    radar, client, client2;
 
 exports['message subscriptions:'] = {
   before: function(done) {
-    var self = this;
     var track = Tracker.create('before', done);
 
     radar = common.spawnRadar();
     radar.sendCommand('start', common.configuration,  function() {
-      self.client = common.getClient('dev', 123, 0, {}, track('client 1 ready'));
-      self.client2 = common.getClient('dev', 246, 0, {}, track('client 2 ready'));
+      client = common.getClient('dev', 123, 0, {}, track('client 1 ready'));
+      client2 = common.getClient('dev', 246, 0, {}, track('client 2 ready'));
 
     });
   },
 
   after: function(done) {
-    this.client.dealloc('test');
-    this.client2.dealloc('test');
+    client.dealloc('test');
+    client2.dealloc('test');
     radar.sendCommand('stop', {}, function() {
       radar.kill();
       done();
@@ -28,118 +27,107 @@ exports['message subscriptions:'] = {
   },
 
   beforeEach: function(done) {
-    this.client.message('test').removeAllListeners();
-    this.client2.message('test').removeAllListeners();
+    client.message('test').removeAllListeners();
+    client2.message('test').removeAllListeners();
     var track = Tracker.create('before each', done);
 
-    this.client.message('test').unsubscribe(track('client unsubscribe'));
-    this.client2.message('test').unsubscribe(track('client2 unsubscribe'));
+    client.message('test').unsubscribe(track('client unsubscribe'));
+    client2.message('test').unsubscribe(track('client2 unsubscribe'));
     common.startPersistence(track('redis cleanup'));
   },
 
   'should subscribe successfully': function(done) {
-    this.client.message('test').subscribe(function() {
+    client.message('test').subscribe(function(message) {
+      assert.equal('subscribe', message.op);
+      assert.equal('message:/dev/test', message.to);
       done();
     });
   },
 
   'should unsubscribe successfully': function(done) {
-    this.client.message('test').unsubscribe(function() {
+    client.message('test').unsubscribe(function(message) {
+      assert.equal('unsubscribe', message.op);
+      assert.equal('message:/dev/test', message.to);
       done();
     });
   },
 
     // sending a message should only send to each subscriber, but only once
   'should receive a message only once per subscriber': function(done) {
-    var client = this.client, client2 = this.client2,
-        message = { state: 'test1'},
-        assertions = 0;
+    var message = { state: 'test1'};
 
-    var wait = function() {
-      assertions += 2;
-      if(assertions == 4) {
-        setTimeout(function() {
-          assert.equal(4, assertions);
-          done();
-        }, 50); //wait 50 ms after all callbacks
+    var finished = {};
+
+    function validate(msg, client_name) {
+      assert.equal('message:/dev/test', msg.to);
+      assert.equal('publish', msg.op);
+      assert.equal(message.state, msg.value.state);
+      assert.ok( !finished[client_name] );
+      finished[client_name] = true;
+      if(finished.client && finished.client2) {
+        setTimeout(done,30);
       }
-    };
+    }
+
 
     client.message('test').on(function(msg) {
-      assert.equal('message:/dev/test', msg.to);
-      assert.equal('publish', msg.op);
-      assert.equal(message.state, msg.value.state);
-      wait();
+      validate(msg, 'client');
     });
     client2.message('test').on(function(msg) {
-      assert.equal('message:/dev/test', msg.to);
-      assert.equal('publish', msg.op);
-      assert.equal(message.state, msg.value.state);
-      wait();
+      validate(msg, 'client2');
     });
 
-    client.message('test').subscribe(function() {
-      client2.message('test').subscribe(function() {
-        client.message('test').publish(message);
-      });
-    });
+    client.message('test').subscribe();
+    client2.message('test').subscribe().publish(message);
   },
 
-  // only subscribers to a channel should be notified of messages to that channel
   'should only receive message when subscribed': function(done) {
-    var client = this.client, client2 = this.client2,
-        message = { state: 'test2'},
-        assertions = 0;
+    //send three messages, client2 will assert if it receieves any,
+    //Stop test when we receive all three at client 1
+
+    var message = { state: 'test1'},
+        message2 = { state: 'test2' },
+        message3 = { state: 'test3' };
 
     client2.message('test').on(function(msg) {
-      if(msg.value.state == 'test2') {
-        assert.ok(false);
-      }
+      assert.ok(false);
     });
 
     client.message('test').on(function(msg) {
-      if(msg.value.state == 'test2') {
-        assert.ok(true);
-        assertions++;
-        if(assertions == 1) {
-          setTimeout(function() {
-            assert.equal(1, assertions);
-            done();
-          }, 50); // 100 ms at most
-        }
+      if(msg.value.state == 'test3') {
+        done();
       }
     });
 
-    client.message('test').subscribe(function() {
-      client.message('test').publish(message);
-    });
-
+    client.message('test').subscribe().publish(message);
+    client2.message('test').publish(message2);
+    client.message('test').publish(message3);
   },
 
-  // unsubscribing should cause that client not to receive messages
   'should not receive messages after unsubscribe': function(done) {
-    var client = this.client, client2 = this.client2;
-    var message = { state: 'test3'};
-    var message2 = { state: 'test4'};
+    //send two messages after client2 unsubscribes,
+    // client2 will assert if it receives message 2 and 3
+    //Stop test when we receive all three at client 1
+
+    var message = { state: 'test1'};
+    var message2 = { state: 'test2'};
+    var message3 = { state: 'test3'};
 
     // test.numAssertions = 3;
     client2.message('test').on(function(msg) {
+      assert.equal(msg.value.state, 'test1');
+      client2.message('test').unsubscribe().publish(message2);
+      client2.message('test').unsubscribe().publish(message3);
+    });
+
+    client.message('test').on(function(msg) {
       if(msg.value.state == 'test3') {
-        client2.message('test').unsubscribe(function() {
-          client.message('test').publish(message2);
-          setTimeout(function() {
-            done();
-          }, 50); // 50 ms at most
-        });
-      }
-
-      if(msg.value.state == 'test4') {
-        assert.ok(false);
+        //received third message without asserting
+        done();
       }
     });
 
-    client2.message('test').subscribe(function() {
-      client.message('test').publish(message);
-    });
+    client2.message('test').subscribe().publish(message);
+    client.message('test').subscribe();
   }
 };
