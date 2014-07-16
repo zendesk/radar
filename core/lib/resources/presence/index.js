e8b1e2052afc271f1a110fe5e00cce82ad261c22
Vanchi Koduvayur <vanchi@zendesk.com>
2014-07-16 13:48:14 -0700

    Presence resource: Use new presence manager
    - Use new presence manager/store/sentry system
    - Remove Local/Remote manager usages
    - Garbage collect set('offline') as first operation:
        - The resource is immediately destroyed, since it was never
          subscribed to.
    - Streamline all log statements

--- core/lib/resources/presence/index.js
+++ core/lib/resources/presence/index.js
@@ -1,12 +1,18 @@
 var Resource = require('../../resource.js'),
-    LocalManager = require('./local_manager.js'),
-    logging = require('minilog')('presence');
+    shasum = require('crypto').createHash('sha1'),
+    PresenceManager = require('./presence_manager.js'),
+    Sentry = require('./sentry.js'),
+    logging = require('minilog')('radar:presence');
 
 var default_options = {
   policy: {
     maxPersistence: 12 * 60 * 60 // 12 hours in seconds
   }
 };
+
+shasum.update(require('os').hostname() + ' ' + Math.random() + ' ' + Date.now());
+var sentryName = shasum.digest('hex').slice(0,15);
+Presence.sentry = new Sentry(sentryName);
 
 function Presence(name, parent, options) {
   Resource.call(this, name, parent, options, default_options);
@@ -19,9 +25,9 @@ Presence.prototype.type = 'presence';
 Presence.prototype.setup = function() {
   var self = this;
 
-  this._xserver = new LocalManager(this.name, this.options.policy);
-  this._xserver.on('user_online', function(userId, userType, userData) {
-    logging.info('user_online', userId, userType);
+  this.manager = new PresenceManager(this.name, this.options.policy, Presence.sentry);
+  this.manager.on('user_online', function(userId, userType, userData) {
+    logging.info('#presence - user_online', userId, userType, self.name);
     var value = {};
     value[userId] = userType;
     self.broadcast({
@@ -31,8 +37,8 @@ Presence.prototype.setup = function() {
       userData: userData,
     });
   });
-  this._xserver.on('user_offline', function(userId, userType) {
-    logging.info('user_offline', userId, userType);
+  this.manager.on('user_offline', function(userId, userType) {
+    logging.info('#presence - user_offline', userId, userType, self.name);
     var value = {};
     value[userId] = userType;
     self.broadcast({
@@ -41,8 +47,8 @@ Presence.prototype.setup = function() {
       value: value
     });
   });
-  this._xserver.on('client_online', function(clientId, userId, userType, userData) {
-    logging.info('client_online', clientId, userId);
+  this.manager.on('client_online', function(clientId, userId, userType, userData) {
+    logging.info('#presence - client_online', clientId, userId, self.name);
     self.broadcast({
       to: self.name,
       op: 'client_online',
@@ -53,8 +59,8 @@ Presence.prototype.setup = function() {
       }
     });
   });
-  this._xserver.on('client_offline', function(clientId, userId, explicit) {
-    logging.info('client_offline', clientId, userId);
+  this.manager.on('client_offline', function(clientId, userId, explicit) {
+    logging.info('#presence - client_offline', clientId, userId, explicit, self.name);
     self.broadcast({
       to: self.name,
       op: 'client_offline',
@@ -65,56 +71,60 @@ Presence.prototype.setup = function() {
       }
     }, clientId);
   });
-
-  // add parent callback
-  this.parentCallback = function() {
-    self._xserver.timeouts();
-  };
-  this.parent.timer.add( this.parentCallback );
 };
 
 Presence.prototype.redisIn = function(message) {
-  try {
-    this._xserver.remoteMessage(message);
-  } catch(e) { return; }
+  logging.info('#presence - incoming from #redis', this.name, message, 'subs:', Object.keys(this.subscribers).length );
+  this.manager.processRedisEntry(message);
 };
 
 Presence.prototype.set = function(client, message) {
-  var self = this,
+  var presence = this,
       userId = message.key;
 
   function ackCheck() {
-    self.ack(client, message.ack);
+    presence.ack(client, message.ack);
   }
 
   if(message.value != 'offline') {
     // we use subscribe/unsubscribe to trap the "close" event, so subscribe now
     this.subscribe(client);
-    this._xserver.addLocal(client.id, userId, message.type, message.userData, ackCheck);
+    this.manager.addClient(client.id, userId, message.type, message.userData, ackCheck);
   } else {
-    // remove from local
-    this._xserver.removeLocal(client.id, userId, ackCheck);
+    if(!this.subscribers[client.id]) { //if this is client is not subscribed
+      //This is possible if a client does .set('offline') without set-online/sync/subscribe
+      Resource.prototype.unsubscribe.call(this, client, message);
+    } else {
+      // remove from local
+      this.manager.removeClient(client.id, userId, message.type, ackCheck);
+    }
   }
 };
 
 Presence.prototype.unsubscribe = function(client, message) {
-  this._xserver.disconnectLocal(client.id);
-  // garbage collect if the set of subscribers is empty
-  if (Object.keys(this.subscribers).length == 1) {
-    this.parent.timer.remove(this.parentCallback);
-  }
+  logging.info('#presence - implicit disconnect', client.id, this.name);
+  this.manager.disconnectClient(client.id);
   // call parent
   Resource.prototype.unsubscribe.call(this, client, message);
 };
 
 Presence.prototype.sync = function(client, message) {
   var self = this;
+  //This is part of the glorious bullshit parade called sync.
+  //Essentially, client_online/online message are not sent to this client until we finish sync,
+  //but we should send a final message with all { userId: userType } as a single online message
+  //nasty stuff. X-(
+  logging.debug('#presence - muting client until sync is completed', client.id, this.name );
+  client.radar_presence_muted = true;
+
   this.fullRead(function(online) {
+    logging.debug('#presence - unmuting client, sync is complete', client.id, self.name );
+    delete client.radar_presence_muted;
     if(message.options && message.options.version == 2) {
       client.send({
         op: 'get',
         to: self.name,
-        value: self._xserver.getClientsOnline()
+        value: self.manager.getClientsOnline()
       });
     } else {
       // will be deprecated when syncs no longer need to use "online" to look like
@@ -137,7 +147,7 @@ Presence.prototype.get = function(client, message) {
       client.send({
         op: 'get',
         to: self.name,
-        value: self._xserver.getClientsOnline()
+        value: self.manager.getClientsOnline()
       });
     } else {
       client.send({
@@ -150,22 +160,30 @@ Presence.prototype.get = function(client, message) {
 };
 
 Presence.prototype.broadcast = function(message, except) {
-  logging.debug('updateSubscribers', message, except);
+  logging.debug('#presence - update subscribed clients', message, except, this.name);
   var self = this;
   Object.keys(this.subscribers).forEach(function(subscriber) {
     var client = self.parent.server.clients[subscriber];
-    if(client && client.id != except) {
+    if(client && client.id != except && !client.radar_presence_muted) {
       client.send(message);
+    } else {
+      logging.warn('#client - not sending: ', (client && client.id), message,  except, (client && client.radar_presence_muted), self.name);
     }
   });
 };
 
 Presence.prototype.fullRead = function(callback) {
-  this._xserver.fullRead(callback);
+  this.manager.fullRead(callback);
+};
+
+Presence.prototype.destroy = function() {
+  this.manager.destroy();
 };
 
 Presence.setBackend = function(backend) {
-  LocalManager.setBackend(backend);
+  PresenceManager.setBackend(backend);
 };
+
+Presence.Sentry = Sentry;
 
 module.exports = Presence;
