f479cf4458b9ff9f341a9c6d8946b2fa4818c7c1
bolddane <pobrien@zendesk.com>
2015-04-13 15:24:53 -0700

    Change Auth to act on messages rather than on resources

---
+++ core/lib/auth.js
@@ -0,0 +1,23 @@
+var log = require('minilog')('radar:auth'),
+    Type = require('./type.js');
+
+function Auth () { };
+
+Auth.authorize = function (message, client, Core) {
+  var rtn = true;
+
+  var options = Type.getByExpression(message.to);
+  if (options) {
+    var provider = options && options.authProvider;
+    if (provider && provider.authorize) {
+      rtn = provider.authorize(options, message, client);
+    }
+  }
+  else {
+    log.info('#authorize: type not defined for name:', message.to);
+  }
+
+  return rtn;
+}
+
+module.exports = Auth;
