ad44bf6c70d3ed68d59628dd1c2fb9b84ac0dc06
Khoa Bui <kbui@zendesk.com>
2019-10-04 13:00:04 +0100

    update javascript-state-machine, mocha, semver, node-fetch, proxyquire

--- src/client/client_session_state_machine.js
+++ src/client/client_session_state_machine.js
@@ -1,34 +1,22 @@
 var StateMachine = require('javascript-state-machine')
 
-var bind = function (fn, context) {
-  return function () {
-    // parameters: event, from, to, ...args
-    var args = Array.prototype.slice.call(arguments, 3)
-    return fn.apply(context, args)
-  }
-}
-
 module.exports.create = function createClientSessionStateMachine (clientSession) {
-  return StateMachine.create({
-    initial: 'initializing',
-    events: [
+  return new StateMachine({
+    init: 'initializing',
+    transitions: [
       { name: 'initialize', from: 'initializing', to: 'ready' },
       { name: 'leave', from: 'ready', to: 'not ready' },
       { name: 'comeback', from: 'not ready', to: 'ready' },
       { name: 'end', from: ['initializing', 'ready', 'not ready'], to: 'ended' }
     ],
-    callbacks: {
-      oninitialize: bind(oninitialize, clientSession),
-      onend: bind(onend, clientSession)
+    methods: {
+      onInitialize: function () {
+        clientSession.emit('initialize')
+      },
+      onEnd: function () {
+        clientSession._cleanup()
+        clientSession.emit('end')
+      }
     }
   })
-}
-
-function oninitialize () {
-  this.emit('initialize')
-}
-
-function onend () {
-  this._cleanup()
-  this.emit('end')
 }
